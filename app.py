15b4f4366b79d4989603ffa036918be09ecda93a
Oscar0807-z <oscarz43@nycstudents.net>
2026-02-25 09:47:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,30 @@ import random
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 random_number = random.choice(my_list)
 number = input("Guess a random number between 1-10")
+
+""" if int(number) != random_number:
+    print ("Incorrect")
+    print (number)
+    x = input("Try again")
+if int(x) != random_number:
+    print ("Incorrect")
+    print (x) and (number)
+    y = input("Try again2")
+if int(y) != random_number: 
+    print ("Incorrect") 
+    print (y) and (x) and (number)
+    z = input("Try again3") """
+
+
+
 while int(number) != random_number:
     print ("Incorrect")
     print (number)
-    input("Try again")
-    break
-if ("Try again") == random_number:
-    print ("Correct")
-if int(number) < random_number:
-    print ("Your number should be higher")
-if int(number) > random_number:
-    print ("Your number should be lower")
+    if int(number) > random_number:
+        print ("Too high")
+    if int(number) < random_number:
+        print ("Too low")
+    number = input("Try again")
+    
+else:
+    print ("Correct") 
