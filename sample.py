13eb0d6ea3b09f188f2beb64ceb1de2999df7786
Oscar0807-z <oscarz43@nycstudents.net>
2026-03-11 15:57:37 -0400

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,17 @@
+
+def money (a,b,c,d):
+    while a>=3:
+        a-=1
+        b+=1
+        a>0
+        a-=1
+        c+=1
+        a-=1
+        d+=1
+        if b == 35:
+            a+=30
+        if c ==100:
+            a+=60
+        if d == 10:
+            a += 9
+    print (b+c+d)
